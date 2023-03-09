057bde6f2856dba928fd0a5ae45a65aa26d0a424
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-09 15:54:52 +0100

    calculete undiscounted user price

--- assets/js/main.js
+++ assets/js/main.js
@@ -26,10 +26,19 @@ function (){
 
     const userAgeEl = document.getElementById ("userAge").value;
     console.log(userAgeEl, "userAge");
+
+    let userPrice = userKmEl * pricePerKm; 
+
+    userPrice = userPrice.toFixed(2);
+    console.log ( `€ ${userPrice} `)
+
+
 }
     
     )
     // moltiplico i chilometri dell'utente per il prezzo per kilometro e lo arrotondo con toFixed
+
+
     // verifico se l'utente ha diritto allo sconto 
         //se è minorenne calcolo il 20% di sconto e lo applico al suo prezzo, quindi lo arrotondo con toFixed e lo stampo in console
             // altrimenti, se over 65 calcolo il 40% di sconto e lo applico al suo prezzo, quindi lo arrotondo con toFixed e lo stampo in console
