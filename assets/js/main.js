9200c98098b4192829984a37f6adb566d292a018
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-09 17:42:36 +0100

    add show ticket effect

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,79 +14,84 @@ const pricePerKm = 0.21
 
 // Creo due input per chiedere all'utente i km e l'età
 // Creo un bottone a cui aggiungo un EventListener e lavoro nella sua funzione anonima
-    // salvo i dati inseriti dall'utente in due variabili e ne stampo in console i value 
-const submitEl = document.getElementById ("submit"); 
+// salvo i dati inseriti dall'utente in due variabili e ne stampo in console i value 
+const submitEl = document.getElementById("submit");
 
-submitEl.addEventListener ("click",
-    
-function (){
+submitEl.addEventListener("click",
 
-    const usernameEl = document.getElementById ("username").value;
-    console.log (usernameEl, "username")
+    function () {
 
-    const userKmEl = document.getElementById ("userKm").value; 
-    console.log (userKmEl, "userKm");
+        //genero il biglietto 
 
-    const userAgeEl = document.getElementById ("userAge").value;
-    console.log(userAgeEl, "userAge");
+        const ticketElement = document.getElementById ("ticket")
+        ticketElement.className = ("d-block")
 
-    //mostro il nome in pagina 
+        const usernameEl = document.getElementById("username").value;
+        console.log(usernameEl, "username")
 
-    document.getElementById ("ticketUsername").innerHTML =  usernameEl;
+        const userKmEl = document.getElementById("userKm").value;
+        console.log(userKmEl, "userKm");
 
-    // produco un numero di carrozza casuale 
+        const userAgeEl = document.getElementById("userAge").value;
+        console.log(userAgeEl, "userAge");
 
-    const carriage = Math.floor(Math.random() * (10 - 1) + 1);
-    document.getElementById ("ticketCarriage").innerHTML =  carriage;
+        //mostro il nome in pagina 
 
-    // produco un numero cp
+        document.getElementById("ticketUsername").innerHTML = usernameEl;
 
-    const cpNumber = Math.floor(Math.random() * (999999 - 000001));
-    document.getElementById ("ticketCode").innerHTML =  cpNumber;
-    
+        // produco un numero di carrozza casuale 
+
+        const carriage = Math.floor(Math.random() * (10 - 1) + 1);
+        document.getElementById("ticketCarriage").innerHTML = carriage;
+
+        // produco un numero cp
+
+        const cpNumber = Math.floor(Math.random() * (999999 - 000001));
+        document.getElementById("ticketCode").innerHTML = cpNumber;
 
 
 
-    // moltiplico i chilometri dell'utente per il prezzo per kilometro e lo arrotondo con toFixed
 
-    let userPrice = userKmEl * pricePerKm; 
+        // moltiplico i chilometri dell'utente per il prezzo per kilometro e lo arrotondo con toFixed
 
-    userPrice = userPrice.toFixed(2);
-    console.log ( `€ ${userPrice}`)
+        let userPrice = userKmEl * pricePerKm;
 
-     // verifico se l'utente ha diritto allo sconto 
-    //se è minorenne calcolo il 20% di sconto e lo applico al suo prezzo, quindi lo arrotondo con toFixed e lo stampo in console
-     if (userAgeEl === "minorenne" ) {
-        const discount20 = userPrice * 0.2; 
-        userPrice = userPrice - discount20;
         userPrice = userPrice.toFixed(2);
-        document.getElementById ("ticketDiscount").innerHTML =  "Biglietto Minori";
-        console.log( `€ ${userPrice} dicountedPrice`);
-        document.getElementById ("ticketPrice").innerHTML =  `€ ${userPrice}`;
+        console.log(`€ ${userPrice}`)
 
-        // altrimenti, se over 65 calcolo il 40% di sconto e lo applico al suo prezzo, quindi lo arrotondo con toFixed e lo stampo in console
-     } else if (userAgeEl === "over65" ) {
+        // verifico se l'utente ha diritto allo sconto 
+        //se è minorenne calcolo il 20% di sconto e lo applico al suo prezzo, quindi lo arrotondo con toFixed e lo stampo in console
+        if (userAgeEl === "minorenne") {
+            const discount20 = userPrice * 0.2;
+            userPrice = userPrice - discount20;
+            userPrice = userPrice.toFixed(2);
+            document.getElementById("ticketDiscount").innerHTML = "Biglietto Minori";
+            console.log(`€ ${userPrice} dicountedPrice`);
+            document.getElementById("ticketPrice").innerHTML = `€ ${userPrice}`;
 
-        const discount40 = userPrice * 0.4; 
-        userPrice = userPrice - discount40;
-        userPrice = userPrice.toFixed(2);
-        document.getElementById ("ticketDiscount").innerHTML =  "Biglietto Over 65";
-        console.log( `€${userPrice} discounted Price`);
-        document.getElementById ("ticketPrice").innerHTML =  `€ ${userPrice}`;
-     } else {
-        document.getElementById ("ticketDiscount").innerHTML =  "Biglietto Standard";
-        document.getElementById ("ticketPrice").innerHTML =  `€ ${userPrice}`;
-     }
-        
+            // altrimenti, se over 65 calcolo il 40% di sconto e lo applico al suo prezzo, quindi lo arrotondo con toFixed e lo stampo in console
+        } else if (userAgeEl === "over65") {
 
-
-}
-    
-    )
+            const discount40 = userPrice * 0.4;
+            userPrice = userPrice - discount40;
+            userPrice = userPrice.toFixed(2);
+            document.getElementById("ticketDiscount").innerHTML = "Biglietto Over 65";
+            console.log(`€${userPrice} discounted Price`);
+            document.getElementById("ticketPrice").innerHTML = `€ ${userPrice}`;
+        } else {
+            document.getElementById("ticketDiscount").innerHTML = "Biglietto Standard";
+            document.getElementById("ticketPrice").innerHTML = `€ ${userPrice}`;
+        }
 
 
 
-   
+    }
+
+)
+
+
+
+
 
 
 // tools
@@ -94,7 +99,7 @@ function (){
 // - const
 // - console.log
 // -* multiplication
-// - / division 
+// - / division
 // if else
 //inner.HTML
 //addEventListener
