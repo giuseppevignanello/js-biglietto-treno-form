f9a8988bcaeb5558d5dd077e2d89a22d5d922a53
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-09 15:51:19 +0100

    add addEventListener and save input value in const

--- assets/js/main.js
+++ assets/js/main.js
@@ -15,6 +15,20 @@ const pricePerKm = 0.21
 // Creo due input per chiedere all'utente i km e l'età
 // Creo un bottone a cui aggiungo un EventListener e lavoro nella sua funzione anonima
     // salvo i dati inseriti dall'utente in due variabili e ne stampo in console i value 
+const submitEl = document.getElementById ("submit"); 
+
+submitEl.addEventListener ("click",
+    
+function (){
+
+    const userKmEl = document.getElementById ("userKm").value; 
+    console.log (userKmEl, "userKm");
+
+    const userAgeEl = document.getElementById ("userAge").value;
+    console.log(userAgeEl, "userAge");
+}
+    
+    )
     // moltiplico i chilometri dell'utente per il prezzo per kilometro e lo arrotondo con toFixed
     // verifico se l'utente ha diritto allo sconto 
         //se è minorenne calcolo il 20% di sconto e lo applico al suo prezzo, quindi lo arrotondo con toFixed e lo stampo in console
