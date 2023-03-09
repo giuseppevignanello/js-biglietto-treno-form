b445a9bb6a96c669498f72c988970fa5de9c1f6c
giuseppevignanello <peppe.vignanello@gmail.com>
2023-03-09 15:42:50 +0100

    add problem analysis

---
+++ assets/js/main.js
@@ -0,0 +1,30 @@
+// Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+// Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+// il prezzo del biglietto è definito in base ai km (0.21 € al km)
+// va applicato uno sconto del 20% per i minorenni
+// va applicato uno sconto del 40% per gli over 65.
+// L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo).
+
+
+
+
+// Salvo in una variabile il prezzo per kilometro
+// Creo due input per chiedere all'utente i km e l'età
+// Creo un bottone a cui aggiungo un EventListener e lavoro nella sua funzione anonima
+    // salvo i dati inseriti dall'utente in due variabili e ne stampo in console i value 
+    // moltiplico i chilometri dell'utente per il prezzo per kilometro e lo arrotondo con toFixed
+    // verifico se l'utente ha diritto allo sconto 
+        //se è minorenne calcolo il 20% di sconto e lo applico al suo prezzo, quindi lo arrotondo con toFixed e lo stampo in console
+            // altrimenti, se over 65 calcolo il 40% di sconto e lo applico al suo prezzo, quindi lo arrotondo con toFixed e lo stampo in console
+
+
+// tools
+
+// - const
+// - console.log
+// -* multiplication
+// - / division 
+// if else
+//inner.HTML
+//addEventListener
+//toFixed
